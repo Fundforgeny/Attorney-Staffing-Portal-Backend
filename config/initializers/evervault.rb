1911eb4ac443dee802cda9cca5eabfd6bf4a4760
Talha-Fakhar <talhafakhar506@gmail.com>
2026-01-01 18:55:26 +0500

    Updating config file for ever vault

--- config/initializers/evervault.rb
+++ config/initializers/evervault.rb
@@ -1,3 +1,3 @@
 # config/initializers/evervault.rb
-Evervault.app_id = "app_98a793684736"
-Evervault.api_key = "ev:key:1:K5QQvX6tgGTZ6TRObC60vaQxwbbOqkRBCPWG0izR5Y3facBmi7QLEW8rr3BA4TOv:a0qonZ:G0ZErx"
+Evervault.app_id = ENV["EVERVAULT_APP_ID"]
+Evervault.api_key = ENV["EVERVAULT_API_KEY"]
