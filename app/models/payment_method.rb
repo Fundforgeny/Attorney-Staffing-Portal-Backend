3fe1e74740be1dfb11360cc0a5b0d5bc46150588
Talha-Fakhar <talhafakhar506@gmail.com>
2026-01-15 12:47:29 +0500

    updating code to get card number

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -5,4 +5,13 @@ class PaymentMethod < ApplicationRecord
   validates :vault_token, presence: true
   validates :stripe_payment_method_id, uniqueness: true, allow_nil: true
   validates :provider, presence: true
+
+  def card_number
+    return nil if vault_token.blank?
+    
+    parsed_token = JSON.parse(vault_token.gsub('=>', ':'))
+    parsed_token["number"] || parsed_token[:number]
+  rescue JSON::ParserError
+    nil
+  end
 end
