d262454e296eed3230f991fa534f2fdc6e20d2f9
titanslaw <contact@titans.law>
2026-05-08 13:00:27 -0400

    Add active archive scopes to payment methods

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -12,18 +12,29 @@ class PaymentMethod < ApplicationRecord
   validates :exp_year, numericality: { only_integer: true, greater_than_or_equal_to: 2000 }, allow_nil: true
   validate :within_payment_method_limit, on: :create
 
-  scope :ordered_for_user, -> { order(is_default: :desc, created_at: :desc) }
+  scope :active, -> { where(archived_at: nil) }
+  scope :archived, -> { where.not(archived_at: nil) }
+  scope :ordered_for_user, -> { active.order(is_default: :desc, created_at: :desc) }
+  scope :ordered_for_admin, -> { order(archived_at: :asc, is_default: :desc, created_at: :desc) }
+
+  def archived?
+    archived_at.present?
+  end
+
+  def archive!
+    update!(archived_at: Time.current, is_default: false)
+  end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["card_brand", "cardholder_name", "created_at", "exp_month", "exp_year", "id", "id_value", "is_default", "last4", "last_updated_via_spreedly_at", "provider", "spreedly_redacted_at", "stripe_payment_method_id", "updated_at", "user_id", "vault_token"]
+    ["archived_at", "card_brand", "cardholder_name", "created_at", "exp_month", "exp_year", "id", "id_value", "is_default", "last4", "last_updated_via_spreedly_at", "provider", "spreedly_redacted_at", "stripe_payment_method_id", "updated_at", "user_id", "vault_token"]
   end
 
   private
 
   def within_payment_method_limit
     return if user.blank?
-    return if user.payment_methods.count < MAX_PAYMENT_METHODS_PER_USER
+    return if user.payment_methods.active.count < MAX_PAYMENT_METHODS_PER_USER
 
-    errors.add(:base, "You can only add up to #{MAX_PAYMENT_METHODS_PER_USER} payment methods")
+    errors.add(:base, "You can only add up to #{MAX_PAYMENT_METHODS_PER_USER} active payment methods")
   end
 end
